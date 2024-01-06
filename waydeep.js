9c1b8ae1aa171abe7a9adb09daebe094e808ba82
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-01-05 21:30:41 -0800

    Add files via upload

---
+++ waydeep.js
@@ -0,0 +1,490 @@
+chrome.contextMenus.create({
+    id:"select",
+    title:"selectwaydeep",
+    contexts: ["selection"]
+})
+chrome.contextMenus.create({
+    id:"0",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"1",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"2",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"3",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"4",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"5",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"6",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"7",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"8",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"9",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"10",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"11",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"12",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"13",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"14",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"15",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"16",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"17",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"18",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"19",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"20",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"21",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"22",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"23",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"24",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"25",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"26",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"27",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"28",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"29",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"30",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"31",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"32",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"33",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"34",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"35",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"36",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"37",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"38",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"39",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"40",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"41",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"42",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"43",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"44",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"45",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"46",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"47",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"48",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"49",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"50",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"51",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"52",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"53",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"54",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"55",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"56",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"57",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"58",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"59",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"60",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"61",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"62",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"63",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"64",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"65",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"66",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"67",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"68",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"69",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"70",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"71",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"72",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"73",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"74",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"75",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"76",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"77",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"78",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"79",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"80",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"81",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"82",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"83",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"84",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"85",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"86",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"87",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"88",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"89",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"90",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"91",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"92",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+chrome.contextMenus.create({
+    id:"93",
+    title:"WayDeep",
+    contexts: ["page"]
+})
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId == "select") {
+        const domain = new URL(tab.url).hostname;
+        const strunged = info.selectionText;
+        const archiveUrl = `https://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&fl=timestamp,original&filter=~urlkey:${strunged}`;
+        chrome.tabs.create({ url: archiveUrl });
+    }
+    else if (info.menuItemId) {
+        const domain = new URL(tab.url).hostname;
+        const strung = `!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz{|}~`
+        const archiveUrl = `https://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&fl=timestamp,original&filter=~urlkey:${strung[info.menuItemId]}`;
+        chrome.tabs.create({ url: archiveUrl });
+  }
+});
